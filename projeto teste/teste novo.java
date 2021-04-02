32691448942f79bd0309ce84f6197cee8d8edf73
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-01 21:31:32 -0300

    Update and rename teste novo to teste novo.java

---
+++ projeto teste/teste novo.java
@@ -0,0 +1,18 @@
+package unikit;
+import java.util.Scanner;
+public class Aplicacao {
+  public static void main(String[]args){
+  menu();
+    /*
+    * @author Lucas
+     */
+}
+    public static void menu() {
+        System.out.println("Seu Feed:\n");
+        System.out.println("1 => Editar Perfil");
+        System.out.println("2 => Ver Amigos");
+        System.out.println("3 => Adicionar Amigos");
+        System.out.println("4 => Enviar Recados");
+        System.out.println("5 => para Sair");
+    }
+}
