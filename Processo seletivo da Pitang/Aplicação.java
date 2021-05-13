1cbbbe88cde9cd90ceeae871d8d9edbd4ef4ac40
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-05-13 06:32:34 -0300

    O código segues essas exigências, abaixo:
    O objetivo desse teste é que você mostre suas habilidades de codificação e o que você valoriza na engenharia de software.
    Acreditamos que um bom código é um código bem testado. Você poderá adicionar e estender a vontade os requisitos da aplicação.
    A aplicação deverá permitir um cadastro de usuários onde teremos as seguintes funcionalidades:
    1. Fazer login
    2. Consultar Usuários (Lista todos)
    3. Incluir Usuário
    4. Alterar Usuário
    5. Remover Usuário
    
    A estrutura do usuário
    • Nome [string]
    • Email [string]
    • Senha[string]
    • Telefones [lista] o ddd [número] o Número [string] o Tipo [string]
    
    Requisitos obrigatórios
    • Utilizar a Plataforma – Java EE
    • Estruturar a aplicação em camadas
    • Uso de banco de dados relacional/SQL (preferencialmente HSQLDB ou H2)
    • Processo de build utilizando Maven Persistência utilizando JDBC ou JPA Utilizar no mínimo Java 8.
    • Utilizar na interface JSF/Primefaces ou JSP com jQuery e Ajax
    • Disponibilizar o código em repositório Git online (Gitlab ou Github)
    • O que você NÃO pode utilizar: Spring / Angular
    
    Requisitos desejáveis
    • Disponibilizar a aplicação em serviço de hospedagem online (Heroku, AWS, Digital Ocean, etc).
    • Uso de EJBs para controle transacional.
    • Uso de servidor de aplicação Wildfly ou OpenLiberty. o Disponibilizar a configuração do servidor de aplicação no repositório Git online junto com o código fonte.
    • Testes unitários com jUnit

---
+++ Processo seletivo da Pitang/Aplicação.java
@@ -0,0 +1,11 @@
+package pitang.pkg01;
+/*
+ * @author valmir
+ */
+public class PitangAplicacao {
+    public static void main(String[] args) {
+        PitangRun social;
+        social = new PitangRun();
+        social.Run();
+    }   
+}
